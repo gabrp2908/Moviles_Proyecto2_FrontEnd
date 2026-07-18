76b3718e3c8f196a5c8aa80cde85ff25f83f42b2
Gabriela Rey <gabrp2908@gmail.com>
2026-07-18 03:37:48 -0400

    fix: visualización de comentarios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'temp_ionic',
+  appName: 'ColapsoGame',
   webDir: 'www'
 };
 
